e3241e80ef269bbd92284268274a3d182800e6f3
AladdinOmar61 <Aladdin.Omar61@gmail.com>
2022-08-01 21:52:10 -0400

    Initialized project
    Began development and started adding template grids for main store page

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="store-title">
+      Mona's Online Store
+      </h1>
+      <div className="item-listings">
+        <div className="store-item"></div>
+        <div className="store-item"></div>
+        <div className="store-item"></div>
+        <div className="store-item"></div>
+        <div className="store-item"></div>
+        <div className="store-item"></div>
+        <div className="store-item"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
